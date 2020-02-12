5cbd69bea9ee3b5b39bc41bad92c1d9176c417d5
Ferry Kranenburg <fkranenburg@yahoo.com>
2020-02-12 14:34:34 +0100

    initial commit

---
+++ nl.c2c.ac.xsp.designer/src/nl/c2c/ac/xsp/XspLibrary.java
@@ -0,0 +1,17 @@
+package nl.c2c.ac.xsp;
+
+import com.ibm.xsp.library.AbstractXspLibrary;
+
+public class XspLibrary extends AbstractXspLibrary {
+
+	@Override
+	public String getLibraryId() {
+		return "nl.c2c.ac.xsp.XspLibrary";
+	}
+
+	@Override
+	public String getPluginId() {
+		return "nl.c2c.ac.xsp.XspLibrary";
+	}
+
+}
